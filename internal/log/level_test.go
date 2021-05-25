42f32d3211810cd2fbc4681b2d9db0e166480811
Jan-Philipp Benecke <jan-philipp@bnck.me>
2021-05-25 23:27:27 +0200

    Chore: Simplify loglevel decode test case

--- internal/log/level_test.go
+++ internal/log/level_test.go
@@ -2,38 +2,16 @@ package log_test
 
 import (
 	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
 	"go.bnck.me/loki-auth-proxy/internal/log"
 	"testing"
 )
 
 func TestDecodeLogLevel(t *testing.T) {
-	bla := log.DecodeLogLevel("bla")
-	if bla != zerolog.InfoLevel {
-		t.Error("Unknown level should match InfoLevel")
-	}
-
-	debug := log.DecodeLogLevel("debug")
-	if debug != zerolog.DebugLevel {
-		t.Error("String debug does not match DebugLevel")
-	}
-
-	info := log.DecodeLogLevel("info")
-	if info != zerolog.InfoLevel {
-		t.Error("String info does not match InfoLevel")
-	}
-
-	warn := log.DecodeLogLevel("warn")
-	if warn != zerolog.WarnLevel {
-		t.Error("String warn does not match WarnLevel")
-	}
-
-	error := log.DecodeLogLevel("error")
-	if error != zerolog.ErrorLevel {
-		t.Error("String error does not match ErrorLevel")
-	}
-
-	fatal := log.DecodeLogLevel("fatal")
-	if fatal != zerolog.FatalLevel {
-		t.Error("String fatal does not match FatalLevel")
-	}
+	assert.Equal(t, zerolog.InfoLevel, log.DecodeLogLevel("bla"), "Unknown level should match InfoLevel")
+	assert.Equal(t, zerolog.DebugLevel, log.DecodeLogLevel("debug"), "String debug does not match DebugLevel")
+	assert.Equal(t, zerolog.InfoLevel, log.DecodeLogLevel("info"), "String info does not match InfoLevel")
+	assert.Equal(t, zerolog.WarnLevel, log.DecodeLogLevel("warn"), "String warn does not match WarnLevel")
+	assert.Equal(t, zerolog.ErrorLevel, log.DecodeLogLevel("error"), "String error does not match ErrorLevel")
+	assert.Equal(t, zerolog.FatalLevel, log.DecodeLogLevel("fatal"), "String fatal does not match FatalLevel")
 }
